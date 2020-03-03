781a75b948bc85cedfee843e496cce385411b986
Nata <38166392+NataKrass@users.noreply.github.com>
2020-03-03 10:50:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	// var $carousel = $('.carousel');
+
+	// $carousel
+	//   .slick() 
+	//   .magnificPopup({
+	// 	type: 'image',
+	// 	delegate: 'a:not(.slick-cloned)',
+	// 	gallery: {
+	// 	  enabled: true
+	// 	},
+	// 	callbacks: {
+	// 	  open: function() {
+	// 		var current = $carousel.slick('slickCurrentSlide');
+	// 		$carousel.magnificPopup('goTo', current);
+	// 	  },
+	// 	  beforeClose: function() {
+	// 		$carousel.slick('slickGoTo', parseInt(this.index));
+	// 	  }
+	// 	}
+	// });
+	$(document).ready(function(){
+		$('.your-class').slick();
+	  });
+	  
+	  
+
+  });
